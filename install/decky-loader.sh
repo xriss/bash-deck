923dcc3a491bf7bda88f833374e122db6c28395b
kriss <kriss@xixs.com>
2023-11-30 13:36:22 +0000

    .

--- install/decky-loader.sh
+++ install/decky-loader.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | bash
+curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
+
+
+
+echo " Press enter to continue. " ; read
 
