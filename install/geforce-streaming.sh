90b534787a83d868f6e68063c771da2051af1341
kriss <kriss@xixs.com>
2023-12-02 00:52:55 +0000

    .

---
+++ install/geforce-streaming.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd `dirname $0`
+
+flatpak install -y com.microsoft.Edge
+flatpak --user override --filesystem=/run/udev:ro com.microsoft.Edge
+
+../js/steamshortcut.js add \
+--app-name="GeForce Streaming" \
+--exe="/usr/bin/flatpak" \
+--launch-options="run com.microsoft.Edge --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25 --start-fullscreen https://play.geforcenow.com/"
+
+
+echo ; echo " finished " ; echo ;
