6664fcfed09c468384288a9a1c8b632a8234d572
kriss <kriss@xixs.com>
2023-12-02 00:46:20 +0000

    .

--- install/gamepass-streaming.sh
+++ install/gamepass-streaming.sh
@@ -7,7 +7,7 @@ flatpak --user override --filesystem=/run/udev:ro com.microsoft.Edge
 ../js/steamshortcut.js add \
 --app-name="Gamepass Streaming" \
 --exe="/usr/bin/flatpak" \
---launch-options="run com.microsoft.Edge --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25"
+--launch-options="run com.microsoft.Edge --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25 --start-fullscreen https://www.xbox.com/play"
 
 
 echo ; echo " finished " ; echo ;
