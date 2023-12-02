30bb988b94ec7ccd16db6cef54b3094e48f7f3e6
kriss <kriss@xixs.com>
2023-12-02 01:37:25 +0000

    .

--- bash/setup-all.sh
+++ bash/setup-all.sh
@@ -3,7 +3,15 @@ cd `dirname $0`
 
 
 
-./install-node.sh
+cat ./github-pull.sh | bash -i
+
+cat ./install-node.sh | bash -i
+
+cat ../install/decky-loader.sh | bash -i
+
+cat ../install/gamepass-streaming.sh | bash -i
+cat ../install/geforce-streaming.sh | bash -i
+cat ../install/luna-streaming.sh | bash -i
 
 
 echo ; echo " finished all " ; echo ;
