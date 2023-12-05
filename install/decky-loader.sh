f413d9715f0a85946d83652f8a9753de051d443c
kriss <kriss@xixs.com>
2023-12-05 22:23:40 +0000

    .

--- install/decky-loader.sh
+++ install/decky-loader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 cd `dirname $0`
 
-
+# an easy one just to test things really
 curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/user_install_script.sh | bash -i
 
 
