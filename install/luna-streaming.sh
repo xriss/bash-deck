f8ff3d0a2ed5c82b8b209bc91796de3826099736
kriss <kriss@xixs.com>
2023-12-07 18:25:01 +0000

    .

--- install/luna-streaming.sh
+++ install/luna-streaming.sh
@@ -12,7 +12,7 @@ flatpak --user override --filesystem=/run/udev:ro com.microsoft.Edge
 ../js/steamshortcut.js add \
 --app-name="Luna Streaming" \
 --exe="/usr/bin/flatpak" \
---launch-options="run com.microsoft.Edge --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25 --start-fullscreen --user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54\" https://luna.amazon.com/"
+--launch-options="run com.microsoft.Edge --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25 --start-fullscreen --user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0\" https://luna.amazon.com/"
 
 
 echo ; echo " finished " ; echo ;
